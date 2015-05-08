49291fcc0487da302b73d4d843fb99edf28eac02
Peter <peterandreoli96@gmail.com>
2015-05-07 23:10:24 -0400

    Main Menu Update \!

--- src/Viewer.java
+++ src/Viewer.java
@@ -2,46 +2,29 @@ import java.awt.Color;
 
 import javax.swing.JFrame;
 
-/**
- * This program uses a menu to display font effects.
- */
+
 public class Viewer {
 	static GamePanel panel;
 	static MainMenu mainMenu;
 	public static String choose;
 	public static JFrame frame = new GameFrame();
+	public static MazeGamePanel mazePanel;
+	public static SnakeGamePanel snakePanel;
 	
 	public static void main(String[] args) {
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Defence Game");
 		mainMenu=new MainMenu();
+		mainMenu.setLayout(null);
 		frame.add(mainMenu);
-		if(choose=="default"){
-			panel = new GamePanel();
-			panel.setBackground(Color.WHITE);
-			MoveKeyBinding.keyBind(panel);
-			panel.setLayout(null);
-			frame.add(panel);
-		}
+		
 		
 		frame.setSize(GameFrame.FRAME_WIDTH, GameFrame.FRAME_HEIGHT);
 		
 
 		frame.setVisible(true);
+		frame.setAlwaysOnTop(true);
+		frame.setResizable(false);
 	}
 }
-/**
-JFrame frame = new GameFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Defence Game");
-		
-		panel = new GamePanel();
-		panel.setBackground(Color.WHITE);
-		MoveKeyBinding.keyBind(panel);
-		panel.setLayout(null);
-		frame.setSize(GameFrame.FRAME_WIDTH, GameFrame.FRAME_HEIGHT);
-		frame.add(panel);
-
-		frame.setVisible(true);
-*/
